b56e1f575624aa37987a7e51a2e1a83179224310
omar jakmira <poweromar19942@gmail.com>
2017-05-31 12:53:39 -0800

    init socket.io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const path = require("path");
 const publicPath = path.join(__dirname,'../public');
 const express = require("express");
+const socketiO = require("socket.io");
+const http = require("http");
+
 const port = process.env.PORT || 1234;
 var app = express();
+var server = http.createServer(app);
+var io = socketiO(server);
+
+io.on('connection',(socket)=>{
+   console.log('new user connected');
+   socket.on('disconnect',()=>{
+      console.log('user disconnected');
+   })
+});
 
 app.use(express.static(publicPath));
-app.listen(port,function () {
+
+server.listen(port,function () {
    console.log(`app runing on ${port}`);
 });
